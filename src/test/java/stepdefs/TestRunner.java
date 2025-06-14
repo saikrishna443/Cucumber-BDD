5bfe94258b784e36991cd8ebf2542a908bcd37dc
saikrishna443 <sai.muttadi@gmail.com>
2025-06-14 20:41:30 +0530

    'second'

--- src/test/java/stepdefs/TestRunner.java
+++ src/test/java/stepdefs/TestRunner.java
@@ -20,7 +20,7 @@ import net.masterthought.cucumber.ReportBuilder;
 		plugin={"pretty","html:target/cucumber-report","json:target/cucumber.json"},
 		features = "src/test/java/", 
 		glue = "stepdefs",
-		tags = {"@sanity"}
+		tags = {"@regression"}
 		//tags = {"not @ignore"}  // it will ignore all the @ignore scenarios and rest all will be executed
 		//tags = {"@login"}  // it will run all the scenarios in login feature file
 		//tags = {"@sanity or @regression"} // it will execute all the scenarios with @sanity or @regression
